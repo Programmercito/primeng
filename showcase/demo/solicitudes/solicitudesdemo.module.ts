225cdd52aa7635b700a1611b28398f75a0f40898
Programmercito <osboceo@live.com>
2017-03-07 19:11:22 -0400

    funcionando

--- showcase/demo/solicitudes/solicitudesdemo.module.ts
+++ showcase/demo/solicitudes/solicitudesdemo.module.ts
@@ -1,18 +1,29 @@
-import {NgModule}     from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {FormsModule,ReactiveFormsModule}    from '@angular/forms';
-import {SolicitudesDemo} from './solicitudesdemo';
-import {SolicitudesDemoRoutingModule} from './solicitudesdemo-routing.module';
-import {SolicitudesService} from './solicitudes.service';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { SolicitudesDemo } from './solicitudesdemo';
+import { SolicitudesDemoRoutingModule } from './solicitudesdemo-routing.module';
+import { SolicitudesService } from './solicitudes.service';
+
+import { HttpModule } from '@angular/http';
+import { InputTextModule, DataTableModule, ButtonModule, DialogModule } from 'primeng/primeng';
 @NgModule({
 	imports: [
 		CommonModule,
-        FormsModule,
-        ReactiveFormsModule,
-		SolicitudesDemoRoutingModule
+		FormsModule,
+		ReactiveFormsModule,
+		SolicitudesDemoRoutingModule,
+		
+		FormsModule,
+		HttpModule,
+		InputTextModule,
+		DataTableModule,
+		ButtonModule,
+		DialogModule
 	],
+	bootstrap: [SolicitudesDemo],
 	declarations: [
 		SolicitudesDemo
 	], providers: [SolicitudesService]
 })
-export class SolicitudesDemoModule {}
+export class SolicitudesDemoModule { }
